dad8508b4b140953838c79fdc3f13a348a41fd55
DESKTOP-S182RU3\ingin <inging_g13679@hotmail.com>
2017-04-27 21:44:12 +0700

    setting

---
+++ src/jenkins_test/Module_A.java
@@ -0,0 +1,11 @@
+package jenkins_test;
+
+public class Module_A {
+	public Module_A(){
+		
+	}
+	public int return_num(){
+		return 1;
+	}
+
+}
---
+++ src/jenkins_test/Module_B_Test.java
@@ -0,0 +1,10 @@
+package jenkins_test;
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class Module_B_Test {
+	@Test
+	public void test(){
+		Module_B module_b = new Module_B();
+		assertEquals("This should return Two",2,module_b.return_int());
+	}
+}
